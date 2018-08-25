35fa508da40d5c6a41679075d1b6ac028eb1ff51
liufeng <zgjllf@msn.cn>
2018-08-25 18:09:12 +0800

    no message

---
+++ Assets/Code/Editor/OrderObjList.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; 
+
+public class OrderObjList : MonoBehaviour {
+
+    //根据当前有没有选中物体来判断可否用快捷键  
+    [MenuItem("Tools/OrderObjList")]  
+    static void OrderSelObjList()  
+    {  
+        GameObject[] go = Selection.gameObjects;
+
+        for (int i = 0; i < go.Length;  i++){
+            go[i].transform.localPosition = GameCommon.GetWorldPos(go[i].transform.parent.position + new Vector3(0,i,0));
+        }
+
+    }  
+  
+}
---
+++ Assets/Code/Game/InGame/Action/MapObjFloatAction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapObjFloatAction : MonoBehaviour {
+    public float scalespeedx = 0.05f;
+    public float scalespeedy = 0.03f;
+
+    public float speedmin = 1f;
+    public float speedmax = 2f;
+
+    public float speed,time;
+    Vector3 baseScale;
+	// Use this for initialization
+	void Start () {
+        
+        this.speed = Random.Range(speedmin,speedmax) ;
+        baseScale = transform.parent.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.time += Time.deltaTime * this.speed;
+
+        float scaleX = baseScale.x - Mathf.Sin(this.time) * this.scalespeedx;
+        float scaleY = baseScale.y + Mathf.Sin(this.time) * this.scalespeedy;
+
+        transform.parent.localScale = new Vector3(scaleX,scaleY,1);
+
+	}
+}

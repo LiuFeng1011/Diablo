3d6ddad8dceb84b8f75573f29550e584b78ad9d6
刘峰 <zgjllf@msn.cn>
2018-09-17 23:17:20 +0800

    no message

--- Assets/Code/Config/Configs/MapGroupConf.cs
+++ Assets/Code/Config/Configs/MapGroupConf.cs
@@ -7,13 +7,22 @@ public class MapGroupConf  {
     public int group;
     public int size;
     public string path;
+    public string way;
 	
 }
 public class MapGroupConfManager
 {
     public List<MapGroupConf> datas = new List<MapGroupConf>();
+
+    //id->data
     public Dictionary<int, MapGroupConf> dataMap = new Dictionary<int, MapGroupConf>();
+
+    //group->data list
     public Dictionary<int, List<MapGroupConf>> dataGroupMap = new Dictionary<int, List<MapGroupConf>>();
+
+    //group->way->data list
+    public Dictionary<int, Dictionary<string,List<MapGroupConf>>> dataWayGroupMap = new Dictionary<int, Dictionary<string, List<MapGroupConf>>>();
+
 
     public void Load()
     {
@@ -31,6 +40,14 @@ public class MapGroupConfManager
                 dataGroupMap.Add(conf.group,new List<MapGroupConf>());
             }
             dataGroupMap[conf.group].Add(conf);
+
+            if(!dataWayGroupMap.ContainsKey(conf.group)){
+                dataWayGroupMap.Add(conf.group, new Dictionary<string, List<MapGroupConf>>());
+            }
+            if(!dataWayGroupMap[conf.group].ContainsKey(conf.way)){
+                dataWayGroupMap[conf.group].Add(conf.way, new List<MapGroupConf>());
+            }
+            dataWayGroupMap[conf.group][conf.way].Add(conf);
         }
     }
 
---
+++ Assets/Code/Game/InGame/GameMap/MapTypeRandomGroup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTypeRandomGroup : MazeMapManager {
+
+    int[,] groundArr;
+
+    /// <summary>
+    /// Inits the map.
+    /// </summary>
+    /// <param name="group">Group type id.</param>
+    /// <param name="row">Row 每行的group数量.</param>
+    /// <param name="col">Col 每列的group数量.</param>
+    public override void InitMap(int group, int row, int col)
+    {
+        base.InitMap(group, row, col);
+
+        groundArr = new int[row, col];
+
+
+        Dictionary<string,List<MapGroupConf>> conflist = ConfigManager.mapGroupConfManager.dataWayGroupMap[group];
+        if (conflist == null && conflist.Count <= 0)
+        {
+            Debug.LogError("no map conf!! group : " + group);
+            return;
+        }
+
+
+
+    }
+}
